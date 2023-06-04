677a952d7d74a068d4b98931adc613e4e70284d3
Braxton Walters <braxton.walters@gmail.com>
2023-06-04 13:16:54 -0400

    Add js file for commenting assignment

---
+++ Programming Basics V22 FT/Intro to Programming/Commenting/code.js
@@ -0,0 +1,7 @@
+// var for keeping count og likes
+var likeCount = 0;
+// func increasing the like counter var
+function increaseLikes() {
+  // Inceasing the counter var by 1
+  likeCount = likeCount + 1;
+}
