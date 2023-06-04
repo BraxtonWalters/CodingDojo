5c109b0efbc437e36331f0b102a158e8cb77254a
Braxton Walters <braxton.walters@gmail.com>
2023-06-04 11:10:37 -0400

    Changed vars in creating vars assignment

--- Programming Basics V22 FT/Intro to Programming/Walters_Braxton_JS_CreateVariables.js
+++ Programming Basics V22 FT/Intro to Programming/Walters_Braxton_JS_CreateVariables.js
@@ -1,8 +1,4 @@
 // Setting the minimum height requirement var
-let minHeight = 42;
+const minHeight = 42;
 // Setting the minimum age requirement var
-let minAge = 10;
-// setting the rider age to an undefined var
-let riderAge;
-// setting the rider height to an undefined var
-let riderHeight;
+const minAge = 10;
