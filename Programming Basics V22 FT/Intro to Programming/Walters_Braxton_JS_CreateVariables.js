dd4c08248f4734f5155535cadb76bb741a3e4d69
Braxton Walters <braxton.walters@gmail.com>
2023-06-04 11:02:34 -0400

    new JS file for the Creating Variables asssignment

---
+++ Programming Basics V22 FT/Intro to Programming/Walters_Braxton_JS_CreateVariables.js
@@ -0,0 +1,8 @@
+// Setting the minimum height requirement var
+let minHeight = 42;
+// Setting the minimum age requirement var
+let minAge = 10;
+// setting the rider age to an undefined var
+let riderAge;
+// setting the rider height to an undefined var
+let riderHeight;
