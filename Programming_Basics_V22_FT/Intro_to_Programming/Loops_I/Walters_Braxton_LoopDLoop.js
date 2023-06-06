29ecabfacd5f223b8bbb68499e26ee789bbcbdd6
Braxton Walters <braxton.walters@gmail.com>
2023-06-06 08:48:39 -0400

    added LoopDLoop assignment js and zip

---
+++ Programming_Basics_V22_FT/Intro_to_Programming/Loops_I/Walters_Braxton_LoopDLoop.js
@@ -0,0 +1,18 @@
+// We would need to count up to 6
+// Start the loop with the iter val set to 2
+// The loop should stop at 6, since its the last piece of candy to dispense
+// The loop iterating var
+// a var for the loop and for the runners speed
+
+// init the runners speed var
+let runnersSpeed = 5.5;
+
+// create a loop that increments by 2 and starts at 2
+for (let i = 2; i <= 6; i += 2) {
+  // condit for checking the runners speed
+  if (runnersSpeed >= 5.5) {
+    console.log("Dispening Candy");
+  }
+}
+
+// I wasn't sure if I should have gave out candy at 6 or stopped.
