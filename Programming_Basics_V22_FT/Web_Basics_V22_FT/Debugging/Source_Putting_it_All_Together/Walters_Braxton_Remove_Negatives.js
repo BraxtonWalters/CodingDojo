cfc284c55b7db8ace0249fabbe6f9e6316a2160f
Braxton Walters <braxton.walters@gmail.com>
2023-06-08 22:15:12 -0400

    Added Debug zip

--- Programming_Basics_V22_FT/Web_Basics_V22_FT/Debugging/Source_Putting_it_All_Together/Walters_Braxton_Remove_Negatives.js
+++ Programming_Basics_V22_FT/Web_Basics_V22_FT/Debugging/Source_Putting_it_All_Together/Walters_Braxton_Remove_Negatives.js
@@ -1,13 +1,15 @@
 // Debug the following code that removes negative values from an array
 
-function removeNegatives(var arr) {
-    for(var i=0; i<=arr.length; i++) {
-        if(arr[i] =< 0) {
-            arr[j] = arr.pop();
-            i--;
+
+// Creating a func 
+function removeNegatives(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] < 0) {
+            arr.splice(i, 1)
+            i--
         }
     }
-    return array;
+    return arr;
 }
 
 var result = removeNegatives([3, 7, -23, 0, 2.5, -4]);
