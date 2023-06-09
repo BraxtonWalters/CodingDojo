5a08a2d768bad893f37298d20948dd0148eef923
Braxton Walters <braxton.walters@gmail.com>
2023-06-09 15:15:13 -0400

    Added Algo Assignment and zip

---
+++ Programming_Basics_V22_FT/Web_Basics_V22_FT/Advanced Arrays II/Walters_Braxton_AC_Min_to_Front.js
@@ -0,0 +1,33 @@
+
+
+// creating a function that takes the min val to the front of an array
+function minToFront(arr) {
+    // init our min and index vars
+    let min = arr[0];
+    let indexMin;
+    // iter through the array 
+    for (let i = 0; i < arr.length; i++) {
+        // if check to find the min and the min's index 
+        if (arr[i] < min) {
+            // setting min var to the current min val
+            min = arr[i];
+            // setting the indexmin var to the current index of the min val
+            indexMin = i;
+        }
+    }
+    // loop to shift the min to the front 
+    for (let j = indexMin; j > 0; j--) {
+        // here we are shift each element of the array
+        arr[j] = arr[j - 1];
+        // setting the min element to the front of the array
+        arr[j - 1] = min;
+    }
+    // returning the array provided
+    return arr
+}
+
+const arr1 = [5, 93, 22, 4];   // 4, 5, 93, 22
+const arr2 = [3, 4, 2, 1, 5];  // 1, 3, 4, 2, 5
+
+
+console.log(minToFront(arr2));
