206eceea2d66da4003029e1ea8c2018b88417f6b
Braxton Walters <braxton.walters@gmail.com>
2023-06-08 23:50:11 -0400

    Added CC Problem Solving file

---
+++ Programming_Basics_V22_FT/Web_Basics_V22_FT/More Problem Solving/Walters_Braxton_CC_Problem_Solving.js
@@ -0,0 +1,12 @@
+
+
+
+// check for key words that would identify if the paragraph is in first, second, or third person.
+
+// create a function that takes one argument called str
+// check the str for 1st person words
+// if str contains those keywords identify it as 1st person
+// if that checks false check for words that would identify it as 2nd person
+// if both return false identify the str as 3rd person
+
+// The hardest part would be ruling out edge cases. for instance if there is a quote with the word I inside a str that written in the 3rd person. Also, someone saying hers/his isn't uncommon in 1st person. 
